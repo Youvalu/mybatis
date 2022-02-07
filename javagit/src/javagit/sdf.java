60e6e4e5625bc596a1d7fb07e99394f533a810ce
Youvalu <21825639@qq.com>
2022-02-07 18:50:39 +0800

    Signed-off-by: Youvalu <21825639@qq.com>

--- javagit/src/javagit/sdf.java
+++ javagit/src/javagit/sdf.java
@@ -8,6 +8,8 @@ public class sdf
 				// TODO Auto-generated method stub
 system.out.println("123");
 system.out.println("Ä£Ñ¹¶¥Ìæ");
+system.out.println("123");
+system.out.println("123");
 			}
 
 	}
